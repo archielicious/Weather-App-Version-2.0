4dfd5eb17f03161e72a5aed10831a1b3d1e58bb1
archie <89709309+aholic-archie@users.noreply.github.com>
2024-06-22 15:01:40 +0530

    Changing the font family to Poppins by using Theme.js

---
+++ src/Theme.js
@@ -0,0 +1,7 @@
+import { createTheme} from "@mui/material";
+let Theme = createTheme({
+  typography: {
+    fontFamily: ["Poppins", "sans-serif"].join(","),
+  },
+});
+export default Theme;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
+import { ThemeProvider } from "@mui/system";
+import Theme from "./Theme";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
