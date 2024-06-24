19c46ec8fd444be9e74cbb0deba3727ef5d199bc
archie <89709309+aholic-archie@users.noreply.github.com>
2024-06-25 01:33:43 +0530

    Modifying the standard convention method for converting wind degrees to wind direction, to centered convention method

--- src/Components/WeatherDisplay.js
+++ src/Components/WeatherDisplay.js
@@ -22,7 +22,7 @@ export default function WeatherDisplay({
   let date = new Date(last_updated * 1000);
   // options are used to format the date, month and year into numeric or string values. Here we are using numeric values for day and year and string value for month
   let options = { day: "numeric", month: "long", year: "numeric" };
-  // below is the method for converting the fetched wind degrees to corresponding wind direction
+  // below is the method for converting the fetched wind degrees to corresponding wind direction, it is the standard comapss rose convention, which refers any degrees from  0 degrees to 22.5 as N, 22.5 degrees to 45 as NNE and so on
   // since there are 16 cardinal directions in the compass each direction would consist 22.5 degrees
   // so for getting the direction from the degees we will divide the given degees by 22.5 and take its floor value to correctly get its corresponding indexed direction
   // we will do modulus 16 to the result so that if the degrees exceed 359.9 and become 360, instead of getting undefined as the direction, the index will start again from 0 means it will start from N again
@@ -45,8 +45,19 @@ export default function WeatherDisplay({
     "NNW",
   ];
 
-  const index = Math.floor(wind_degree / 22.5) % 16;
-  let wind_direction = cardinalDirections[index];
+  // let standard_index = Math.floor(wind_degree / 22.5) % 16;
+  // let wind_direction = cardinalDirections[standard_index];
+
+  // below is another method for converting wind degrees to wind direction, it is the centered compass rose convention, which refers to any degrees between 348.75 to 11.25 as N, 11.25 to 33.75 as NNE and so on
+  // first we will add 11.25 to the given degrees to bring it to the range for the centered convention
+  // then we will divide the centered convention adjusted degrees by 22.5 and take the floor value value to get the corresponding indexed direction
+  // we will do modulus 16 to the resulted index so that a resulted index greater than 15 will again start from 0 that means from N
+  let centered_convention_degrees = wind_degree + 11.25;
+  let centered_convention_corresponding_index =
+    Math.floor(centered_convention_degrees / 22.5) % 16;
+  let wind_direction =
+    cardinalDirections[centered_convention_corresponding_index];
+
   return (
     <>
       <Typography variant="h5">
